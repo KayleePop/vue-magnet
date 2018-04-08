31c1b5611fbf1d20b249a49cfbd4be344a7ed07c
Kaylee <34007889+KayleePop@users.noreply.github.com>
2018-04-08 15:46:30 -0500

    Added fixture seeding for space.jpg
    A seperate instance of WebTorrent is used to seed the file in order to
    test without needing external seeding.

--- test/rebind.js
+++ test/rebind.js
@@ -3,6 +3,8 @@ const loadjs = require('loadjs')
 const VueMagnet = require('../vue-magnet.js')
 const test = require('tape')
 const utils = require('./shared/utils.js')
+const FS = require('fs')
+const WebTorrent = require('webtorrent')
 
 test('rebinding the magnet link should correctly load the new image', (t) => {
   utils.cleanUp()
@@ -11,14 +13,25 @@ test('rebinding the magnet link should correctly load the new image', (t) => {
   t.plan(1)
   t.timeoutAfter(20 * 1000)
 
+  let spaceImg64 = FS.readFileSync(require.resolve('./fixtures/space.jpg'), 'base64')
+  let spaceBuffer = Buffer.from(spaceImg64, 'base64')
+  spaceBuffer.name = 'space.jpg'
+
+  let client = new WebTorrent()
+  client.seed(spaceBuffer)
+
   loadjs('https://vuejs.org/js/vue.js', () => {
     window.Vue.use(VueMagnet)
 
-    window.Vue.config.errorHandler = (err) => t.fail(err.message)
+    window.Vue.config.errorHandler = (err) => {
+      client.torrents[0].destroy(() => client.destroy())
+      t.fail(err.message)
+    }
 
     window.vueInstance = new window.Vue({
       el: '#app',
       render: (h) => h(RebindComponent, { on: { success () {
+        client.torrents[0].destroy(() => client.destroy())
         t.pass()
       }}})
     })
--- test/rebind.vue
+++ test/rebind.vue
@@ -11,7 +11,7 @@ export default {
       // Sintel magnet link
       currentMagnet: 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent&path=poster.jpg',
       // from fixtures/space.torrent
-      spaceMagnet: 'magnet:?xt=urn:btih:2e95238d08ef2cc462e8eeb215b5f0f093b1b205&dn=space.jpg&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com',
+      spaceMagnet: 'magnet:?xt=urn:btih:4833c9743ec9f34133c2b6b24ef8686aa269e621&dn=space.jpg&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com',
       firstLoaded: false
     }
   },
